71a8c1cf2f7285199fca1efe80e577b885526fba
ErbolTakhirov <takhiroverbol@gmail.com>
2025-12-27 20:28:58 +0600

    Update settings.py

--- sb_finance/settings.py
+++ sb_finance/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dev-secret-key-change-me')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost', 'sb-english.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,sb-english.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
